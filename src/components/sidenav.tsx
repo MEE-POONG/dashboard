50b4e47bc9e146f65e6d4a6ede6bb924b6b55b13
devilzeros <devilzeros@outlook.co.th>
2024-01-13 17:11:24 +0700

    est

--- src/components/sidenav.tsx
+++ src/components/sidenav.tsx
@@ -161,7 +161,7 @@ const SideNav: React.FC<SidenavProps> = ({ openSidebar }) => {
                 </div>
             </div>
 
-            <aside className={`w-60 md:w-64 ${isMaxSidebar ? '' : '-translate-x-48'} fixed transition transform ease-in-out duration-1000 z-50 flex h-screen bg-[#1E293B]`}>
+            <aside className={`w-60 md:w-60 ${isMaxSidebar ? '' : '-translate-x-48'} fixed transition transform ease-in-out duration-1000 z-50 flex h-screen bg-[#1E293B]`}>
                 {/* <!-- open sidebar button --> */}
                 <div className="max-toolbar translate-x-24 scale-x-0 w-full -right-6 transition transform ease-in duration-300 flex items-center justify-between border-4 border-white dark:border-[#0F172A] bg-[#1E293B]  absolute top-2 rounded-full h-12">
                     <div className="flex pl-4 items-center space-x-2">
@@ -178,13 +178,13 @@ const SideNav: React.FC<SidenavProps> = ({ openSidebar }) => {
                             </div>
                         </div >
                     </div>
-                    <div className="flex items-center space-x-3 group bg-gradient-to-r dark:from-cyan-500 dark:to-blue-500 from-indigo-500 via-purple-500 to-purple-500  pl-10 pr-1 py-1 rounded-full text-white">
+                    <div className="flex items-center space-x-3 group bg-gradient-to-r dark:from-cyan-500 dark:to-blue-500 from-indigo-500 via-purple-500 to-purple-500  pl-4 pr-1 py-1 rounded-full text-white">
                         <div className="transform ease-in-out duration-300 mr-11 md:mr-12 font-bold text-sm md:text-base">
                             MNR - Dashboard
                         </div>
                     </div>
                 </div>
-                <div onClick={openNav} className="-right-7 md:-right-6 transition transform ease-in-out duration-500 flex border-4 border-white dark:border-[#0F172A] bg-[#1E293B] dark:hover:bg-blue-500 hover:bg-purple-500 absolute top-2 p-3 rounded-full text-white hover:rotate-45">
+                <div onClick={openNav} className="-right-7 md:-right-5 transition transform ease-in-out duration-500 flex border-4 border-white dark:border-[#0F172A] bg-[#1E293B] dark:hover:bg-blue-500 hover:bg-purple-500 absolute top-2 p-3 rounded-full text-white hover:rotate-45">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-4 h-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
                     </svg>
@@ -195,7 +195,7 @@ const SideNav: React.FC<SidenavProps> = ({ openSidebar }) => {
                     {navigationItems.map((item, index) => (
                         <div
                             key={index}
-                            className="hover:ml-4 w-full text-white hover:text-purple-500 dark:hover:text-blue-500 bg-[#1E293B] p-2 pl-8 rounded-full transform ease-in-out duration-300 flex flex-row items-center space-x-3">
+                            className="hover:ml-4 w-full text-white hover:text-purple-500 dark:hover:text-blue-500 bg-[#1E293B] p-2 pl-4 rounded-full transform ease-in-out duration-300 flex flex-row items-center space-x-3">
                             {item.icon}
                             <Link href={item.href}>
                                 {item.text}
